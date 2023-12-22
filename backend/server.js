2db8595544326a90c09dc08b727daac8b5c69e9c
Dogus Guleryuz <dogusguleryuz@gmail.com>
2023-12-22 04:05:49 -0800

    config port fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const router = require("./routes/router");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({ credentials: true, origin: true }));
